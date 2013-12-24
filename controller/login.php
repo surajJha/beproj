71e1369cc6c26d3ca802800dd285ee73407efb89
DELL <DELL@DELL-PC>
2013-12-24 18:25:48 +0530

    commit from khushboo loginajfghj

--- controller/login.php
+++ controller/login.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../model/database.php");
 
+echo "kjwdgwhsbgqdhnxwd";
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
