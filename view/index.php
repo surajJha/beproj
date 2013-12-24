a8812bef6a97f9e9fb83b87892a5082d7c8a394f
suraj <suraj@suraj-PC>
2013-12-25 00:12:38 +0530

    index change

--- view/index.php
+++ view/index.php
@@ -41,41 +41,26 @@
 <style type="text/css">
 body{padding-top:20px;}
 </style>
+<div id="fullscreen_bg" class="fullscreen_bg"/>
+
 <div class="container">
-    <div class="row">
-    <div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Please sign in</h3>
-        </div>
-          <div class="panel-body">
-              <form accept-charset="UTF-8" role="form" method="post" action="../controller/login.php">
-                    <fieldset>
-                <div class="form-group">
-                  <input class="form-control" placeholder="Username" name="username" type="text">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password" value="">
-              </div>
-              <div class="checkbox">
-                  <label>
-                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                  </label>
-                </div>
-              <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
-            </fieldset>
-              </form>
-          </div>
-      </div>
-    </div>
-  </div>
+
+  <form class="form-signin" method="post " action = "login.php">
+    <h1 class="form-signin-heading text-muted"><b> Comperio</b>   </h1>
+    <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+
+    <input type="password" class="form-control" placeholder="Password" required="">
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+      Sign In
+    </button>
+  </form>
+
 </div>
 <!-- ****************************************************** -->
 
 
 
-
-   <!--*********************FOOTER CODE *****************  -->
+<!--*********************FOOTER CODE *****************  -->
     <div class="container">
 
       <hr>
@@ -87,7 +72,7 @@ body{padding-top:20px;}
           </div>
         </div>
       </footer>
-
+</div>
       <!-- ************************************************** -->
 
 
@@ -101,4 +86,6 @@ body{padding-top:20px;}
     <script src="../lib/theme/js/modern-business.js"></script>
 
   </body>
+
+   
 </html>
