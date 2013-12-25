f5feedd6cd7aff74ad8d968a17bae025686005d7
DELL <DELL@DELL-PC>
2013-12-25 13:49:57 +0530

    added functions password_encrypt generate_salt and check_password

--- controller/login.php
+++ controller/login.php
@@ -1,24 +1,19 @@
 <?php
 require_once("../model/database.php");
-
-echo "woo";
+require_once ("functions.php");
 
 $username = $_POST["username"];
 $password = $_POST["password"];
+
 
 //$hash_password=null;
 
 $query="SELECT * FROM user WHERE username = '".$username."' and password='".$password."'";
 $result=mysqli_query($connection, $query);
 
-if(is_null($result))
-{
-    //echo "Login not successful";
-    
-    //redirect to homepage with error
-}
- else {
-    //redirect to new page 
-}
- 
+$hash = password_encrypt($password);
+echo $hash;
+
+
+
 ?>
