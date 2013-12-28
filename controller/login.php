964b1243b49b2a3ecc735e7f3c1788d3aa189cb7
suraj <suraj@suraj-PC>
2013-12-28 15:21:30 +0530

    Merge origin/master
    Conflicts:
    	controller/login.php

--- controller/login.php
+++ controller/login.php
@@ -1,24 +1,43 @@
 <?php
+
 session_start();
 require_once("../model/database.php");
 require_once ("functions.php");
 
+//username and password from login form 
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-
-//$hash_password=null;
-
-$query="SELECT * FROM user ";
-$result=mysqli_query($connection, $query);
+//extract existing hash from database
+$query = "SELECT * FROM user WHERE user_id='{$username}'";
+$result = mysqli_query($connection, $query);
 $row = mysqli_fetch_assoc($result);
-//var_dump($row);
+$existing_hash = $row["password"];
 
-?>
+//checking if password matches existing hash and redirect accordingly
+if (password_check($password, $existing_hash))
+{
+    //tors -> teacher or student
+    //$tors=$row["tors"];
+    
+    //login successful
+    if($username === "admin")
+    {
+        //header("Location:http://../adminpage.php");
+    }
+    else if (1)
+    {
+        //header("Location: teacher");
+    }
+    else 
+        {
+        //header("Location: student");
+    }
+} 
+else 
+{
+    //login unsuccessful
 
-$hash = password_encrypt($password);
-echo $hash;
-
-
-
+    header("Location:http://localhost/beproj/view/index.php");
+}
 ?>
