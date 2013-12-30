622670556f5d1d75a9ca226412116635975f7ade
suraj <suraj@suraj-PC>
2013-12-30 19:16:38 +0530

    Merge origin/master
    Conflicts:
    	controller/login.php

---
+++ view/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <p class="navbar-text">This is the footer.</p>
+        <ul class="nav navbar-nav navbar-right">
+
+            <li><a href="#">Contact Us</a></li>
+
+        </ul>
+    </div>
+</div>
---
+++ view/header.php
@@ -0,0 +1,24 @@
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class =" container">
+                <a href="#" class =" navbar-brand">HexaGraph</a>
+
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Social</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Logout <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Go Back To HomePage</a></li>
+                                <li><a href="#">Do Nothing</a></li>
+                                <li><a href="#">Whatever</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+ 
