0290ff847d25acca01205bf786b239a7c0c7d922
DELL <DELL@DELL-PC>
2014-01-04 11:28:00 +0530

    -formatted

--- controller/allStudents.js
+++ controller/allStudents.js
@@ -8,29 +8,29 @@ $(document).ready(function(){
     
     $('#overview').click(function(){
       
- $.ajax({
-      url: "../model/allStudents.php",
-      type: "POST",     //must be capital letters
-      data:"{}",        // send request data to php file
-  datatype: 'json',
-      success: function(data){ 
-          //alert("fssaklnsgsn");
-          //displaying data in table format
-   var tr;
-    for (var i = 0; i < data.length; i++) {
-        tr = $('<tr/>');
-        tr.append("<td>" + data[i].user_id + "</td>");
-        tr.append("<td>" + data[i].password + "</td>");
-        tr.append("<td>" + data[i].fname + "</td>");
-        $('#mytable').append(tr);
-    }
-                        },
-      error:function(){
-        //  alert('FAIL');
-          $("#result").html('There was an error updating the settings');
-          $("#result").addClass('msg_error');
-          $("#result").fadeIn(1500);
-      }   
-    }); 
-});
+        $.ajax({
+            url: "../model/allStudents.php",
+            type: "POST",     //must be capital letters
+            data:"{}",        // send request data to php file
+            datatype: 'json',
+            success: function(data){ 
+                //alert("fssaklnsgsn");
+                //displaying data in table format
+                var tr;
+                for (var i = 0; i < data.length; i++) {
+                    tr = $('<tr/>');
+                    tr.append("<td>" + data[i].user_id + "</td>");
+                    tr.append("<td>" + data[i].password + "</td>");
+                    tr.append("<td>" + data[i].fname + "</td>");
+                    $('#mytable').append(tr);
+                }
+            },
+            error:function(){
+                //  alert('FAIL');
+                $("#result").html('There was an error updating the settings');
+                $("#result").addClass('msg_error');
+                $("#result").fadeIn(1500);
+            }   
+        }); 
     });
+});
