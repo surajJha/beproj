8033a67c90bf797f92ded9bdf12ac87b87c7252c
suraj <suraj@suraj-PC>
2014-01-19 15:50:44 +0530

    Merge origin/master
    Conflicts:
    	view/teacher/teacher_edit_profile.php

--- model/student/test_process.php
+++ model/student/test_process.php
@@ -1,46 +1,59 @@
 <?php
-    session_start();
-    require_once '../database.php';
-    
-    $id = $_SESSION['test_id'];
-      $code = $_POST['test_code'];
-   
-   
-$query1 = "select * from test where test_id='{$id}' and test_code='{$code}'";
 
-      
-      
-      
-      
-      
-      
-      
-      
-    $result = mysqli_query($connection, $query);
-    $row = mysqli_fetch_assoc($result);
+session_start();
+require_once '../database.php';
+
+$id = $_SESSION['test_id'];
+$code = $_POST['test_code'];
+// savin entire test in session variable
+$test = array();
+
+$query = "select * from test where test_id='{$id}' and test_code='{$code}'";
+if ($res = mysqli_query($connection, $query)) {
     
-    if(// test cond is true)
-    {
-        if(test is random)
-        {
-            random_test();
-        }
-        else if(test is custom)
-        {
-            custom_test();
-        }
+    $row = mysqli_fetch_assoc($res);
+    $test = $row;
+    print_r($test);
+
+    // $result = mysqli_query($connection, $query);
+    // $row = mysqli_fetch_assoc($result);
+
+    if ($test['random'] === '1') {
+        // random test logic
+        random_test();
+    } else if ($test['random'] === '0') {
+        // custom test logic
+        custom_test();
     }
-    
-    else
-    {
-        echo "Entered test code doesn't match with the database code. Please Try Again.";
+}
+// test condition has failed
+else {
+    echo "Entered test code doesn't match with the database code. Please Try Again.";
+}
+
+// logic for various types of test
+function random_test() {
+    $query = "SELECT q.question_id, q.question_desc, q.type"
+            ."FROM question AS q"
+            ."WHERE q.standard= '{$test['standard']}' AND q.subject_name='{$test['subject_name']}'"
+            ."AND q.topic_name IN (SELECT t.topic_name FROM test_on_topic AS t WHERE t.test_id= '{$test['test_id']}')";
+            
+            
+            
+    if ($res = mysqli_query($connection, $query)) {
+
+        while($row = mysqli_fetch_assoc($res))
+            $questions=$row;
     }
-    
-    // logic for various types of test
-    function random_test()
-    {
-        
+}
+/*
+function custom_test() {
+    $query = "";
+    if ($res = mysqli_query($connection, $query)) {
+
+        $row = mysqli_fetch_assoc($res);
     }
-    
-    
+}
+ * */
+ 
 ?>
