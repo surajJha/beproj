188564513ad3f2d7a70930d32d6f43c8b72dcd3b
DELL <DELL@DELL-PC>
2014-01-20 10:57:11 +0530

    custom questions!

--- model/teacher/select_custom_questions.php
+++ model/teacher/select_custom_questions.php
@@ -1,7 +1,28 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+header('Content-Type: application/json');
+
+session_start();
+
+require_once '../database.php';
+
+$x = array();
+
+$query = "SELECT q.question_id, q.type, q.question_desc
+                    FROM question AS q
+                    WHERE q.standard='{$_POST['standard']}'
+                    AND q.subject_name='{$_POST['subject']}'  
+                    AND ";
+
+$result = mysqli_query($connection, $query);
+
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($x, $row);
+    }
+} else {
+    $x[]="There are no questions that match your selection!";
+}
+
+echo json_encode($x);
 ?>
