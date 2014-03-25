65e8d8ba74c244a196eb2637267f2f8ed7fde7db
suraj <suraj@suraj-PC>
2014-03-25 12:25:54 +0530

    Merge origin/master
    Conflicts:
    	view/student/test_result.php

--- controller/student/testcharts.js
+++ controller/student/testcharts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-
-   
+// one ajax method plus chart specefic functions for each type of chart
+ //***************************************POLAR*******************************************************  
     $.ajax({
         type: 'GET',
         url: '../../model/student/testcharts.php',
@@ -111,6 +111,177 @@ $(document).ready(function() {
         var chart = new Highcharts.Chart(options);
 
     }
+    
+    //*******************SCATTER PLOT CODE START HERE*******************************************************************************
+
+ $.ajax({
+        type: 'GET',
+        url: '../../model/student/testcharts.php',
+        success: function(scatter_data)
+        {
+            scatterplot(scatter_data);
+        },
+        error: function()
+        {
+            alert("Error");
+        }
+    });
+
+
+
+
+function scatterplot(scatter_data)
+{
+
+        $('#container').highcharts({
+            chart: {
+                type: 'scatter',
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Height Versus Weight of 507 Individuals by Gender'
+            },
+            subtitle: {
+                text: 'Source: Heinz  2003'
+            },
+            xAxis: {
+                title: {
+                    enabled: true,
+                    text: ''
+                },
+                startOnTick: true,
+                endOnTick: true,
+                showLastLabel: true
+            },
+            yAxis: {
+                title: {
+                    text: 'Weight (kg)'
+                }
+            },
+
+            // set parameters for the male/female box in the top left
+            legend: {
+                layout: 'vertical',
+                align: 'left',
+                verticalAlign: 'top',
+                x: 100,
+                y: 70,
+                floating: true,
+                backgroundColor: '#FFFFFF',
+                borderWidth: 1
+            },
+            plotOptions: {
+                scatter: {
+                    marker: {
+                        // radius of the bubbles
+                        radius: 5,
+                        states: {
+                            hover: {
+                                enabled: true,
+                                lineColor: 'rgb(100,100,100)'
+                            }
+                        }
+                    },
+                    states: {
+                        hover: {
+                            marker: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '<b>{series.name}</b><br>',
+                        pointFormat: '{point.x} cm, {point.y} kg'
+                    }
+                }
+            },
+            series: [{
+                name: 'Female',
+                color: 'rgba(223, 83, 83, .5)',
+                data: dat
+    
+            }, {
+                name: 'Male',
+                color: 'rgba(119, 152, 191, .5)',
+                data: sdg
+            }]
+        });
+  
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /* function barChart_topicWisePerformance(data)
