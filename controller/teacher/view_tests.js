c7d1e250edd25b760420d16afdb8c5c50887a3e2
DELL <DELL@DELL-PC>
2014-04-06 17:15:40 +0530

    all results for teacher done

--- controller/teacher/view_tests.js
+++ controller/teacher/view_tests.js
@@ -17,15 +17,22 @@ $(document).ready(function() {
                     else
                     {
 
-                        var t = "<div class=\"col-lg-8\"><h3><strong>Upcoming Tests</strong></h3><hr/><div class=\"table-responsive\"><table class=\"table table-striped\">"
+                        var t = "<br><div class=\"col-lg-9\"><div class=\"panel panel-info\" ><div class=\"panel-heading\"><h3><strong>Upcoming Tests</strong></h3></div><div class=\"panel-body\"><div class=\"table-responsive\"><table class=\"table table-striped\">"
                         t += "<thead><tr><th>Test-id</th> <th>Subject</th> <th>Class</th><th>Test Name</th> <th>Date</th> <th>Duration</th> <th>Test Code</th> </tr></thead>";
                         for (var i = 0; i < data.length; i++) {
 
-                            t += "<tr><td>" + data[i].test_id + "</td> <td>" + data[i].subject_name + "</td><td>" + data[i].standard + "-" + data[i].division + "</td><td>" + data[i].test_name + "</td><td>" + data[i].date + "</td><td>" + data[i].duration + " <td> " + data[i].test_code + "</td></tr>";
+                            t += "<tr><td>" + data[i].test_id + "</td> <td>" + data[i].subject_name + "</td><td>" + data[i].standard + "-" + data[i].division + "</td><td>" + data[i].test_name + "</td><td>" + data[i].date + "</td><td>" + data[i].duration + " <td> " + data[i].test_code + "</td><td><button class=\"up_test_button btn btn-primary\" value=\"" + data[i].test_id + "\">View</button></td></tr>";
                         }
 
-                        t += "</table></div></div>";
+                        t += "</table></div></div></div></div>";
                         $("#test_table").html(t);
+                        
+                        
+                        $(".up_test_button").click(function() {
+                            var id = $(this).val();
+                            url = "http://localhost/beproj/view/teacher/view_test.php?test_id=" + id;
+                            window.open(url, '_blank');
+                        });
                     }
                 },
                 error: function()
@@ -51,14 +58,14 @@ $(document).ready(function() {
                     else
                     {
 
-                        var t = "<div class=\"col-lg-8\"><h3><strong>Previous Tests</strong></h3><hr/><div class=\"table-responsive\"><table class=\"table table-striped\">"
+                        var t = "<br><div class=\"col-lg-9\"><div class=\"panel panel-success\" ><div class=\"panel-heading\"><h3><strong>Previous Tests</strong></h3></div><div class=\"panel-body\"><div class=\"table-responsive\"><table class=\"table table-striped\">"
                         t += "<thead><tr><th>Test-id</th> <th>Subject</th> <th>Class</th><th>Test Name</th> <th>Date</th> <th>Duration</th> <th>Test Code</th> </tr></thead>";
                         for (var i = 0; i < data.length; i++) {
 
                             t += "<tr><td>" + data[i].test_id + "</td> <td>" + data[i].subject_name + "</td><td>" + data[i].standard + "-" + data[i].division + "</td><td>" + data[i].test_name + "</td><td>" + data[i].date + "</td><td>" + data[i].duration + " <td> " + data[i].test_code + "</td> <td><button class=\"prev_test_button btn btn-success\" value=\"" + data[i].test_id + "\">Result</button></td></tr>";
                         }
 
-                        t += "</table></div></div>";
+                        t += "</table></div></div></div></div>";
                         $("#prev_table").html(t);
 
                         $(".prev_test_button").click(function() {
