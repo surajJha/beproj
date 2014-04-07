47bc2104fc7e5c3c797c419e77f38ac567d284b5
DELL <DELL@DELL-PC>
2014-04-07 12:24:15 +0530

    all results for teacher done

--- controller/student/results.js
+++ controller/student/results.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
                                             },
                                             success: function(data)
                                             {
-                                                //lineChart_TestWiseAnnualPerformance(data);
+                                                columnBasic_studentPerformance(data);
                                             }
                                         });
                             });
@@ -96,7 +96,8 @@ $(document).ready(function() {
                                         type: 'GET',
                                         url: '../../model/student/results.php',
                                         data: {
-                                            f: 'get_test_id'
+                                            f: 'get_test_id',
+                                            subj: $("#subject").val()
                                         },
                                         success: function(j)
                                         {
@@ -124,18 +125,69 @@ $(document).ready(function() {
                                                 {
                                                     e.preventDefault();
 
-                                                    $.ajax(
-                                                            {
-                                                                type: 'GET',
-                                                                url: '../../model/student/results.php',
-                                                                data: {
-                                                                    f: 'subject'
-                                                                },
-                                                                success: function(data)
+                                                    var t = "<div class=\"row\"><div class=\"col-lg-offset-2 col-lg-8\" id=\"test_summary\"></div><div class=\"row\"><div class=\"col-lg-3\" id=\"chart1\"></div><div class=\"col-lg-9\" id=\"chart2\"></div></div><div class=\"row\"><div class=\"col-lg-offset-2 col-lg-8\" id=\"student_ranks\"></div></div><div class=\"row\"><div class=\"col-lg-offset-2 col-lg-8\" id=\"question_details\"></div></div>";
+                                                    $("#myContent").html(t);
+
+                                                    /////*********************
+
+                                                    var f = 't';
+                                                    var test_id = $("#tc").val();
+                                                    $.ajax({
+                                                        type: 'GET',
+                                                        data: {f: f,
+                                                            test_id: test_id},
+                                                        url: '../../model/student/results.php',
+                                                        success: function(data)
+                                                        {
+                                                            var str = "";
+                                                            str += "<div class=\"row\"><div class=\"col-lg-offset-2 col-lg-5\">";
+                                                            str += "<b>Test id : </b> " + test_id + "<br/>";
+                                                            str += "<b>Subject : </b> " + data.subject_name + "<br/>";
+                                                            str += "<b>Class : </b> " + data.standard + " - " + data.division + "<br/>";
+                                                            str += "</div><div class=\"col-lg-5\">";
+                                                            str += "<b>Date : </b> " + data.date + "<br/>";
+                                                            str += "<b>Duration : </b> " + data.duration + " mins<br/>";
+                                                            str += "</div></div>";
+                                                            str += "<hr>";
+                                                            $("#test_summary").html(str);
+                                                        }
+                                                    });
+
+
+                                                    var f = 'q';
+                                                    $.ajax({
+                                                        type: 'GET',
+                                                        data: {f: f,
+                                                            test_id: test_id},
+                                                        url: '../../model/teacher/results.php',
+                                                        success: function(data)
+                                                        {
+
+                                                            var t = "<hr/><div class=\"table-responsive\"><table class=\"table table-striped\"><caption><h3>Questions</h3></caption>"
+                                                            t += "<thead><tr> <th>Question No.</th> <th>Type</th><th>Description</th><th>Your Response</th><th>Correct Answer</th></tr></thead>";
+
+                                                            for (var i = 0; i < data.length; i++) {
+                                                                t += "<tr> <td>" + (i + 1) + "</td><td>" + data[i].type + "</td><td><details><summary>" + data[i].question_desc + "</summary><br/>";
+                                                                if (data[i].type == "Mcq")
                                                                 {
-                                                                    //lineChart_TestWiseAnnualPerformance(data);
+                                                                    t += "<p><b> A: </b> " + data[i].mcq['optionA'] + "&nbsp;&nbsp;&nbsp; <b>B:</b> " + data[i].mcq['optionB'] + " &nbsp; &nbsp; &nbsp; <b> C: </b> " + data[i].mcq['optionC'] + "&nbsp;&nbsp;&nbsp; <b>D:</b> " + data[i].mcq['optionD'] + " </p>";
                                                                 }
-                                                            });
+                                                                t += "</details></td><td>" + data[i].response + "</td><td>" + data[i].answer + "</td></tr>";
+                                                            }
+                                                            t += "</table></div>";
+                                                            $("#question_details").html(t);
+
+
+                                                            //*********************Charts*******************          
+                                                            barChart_topicWisePerformance(data);
+                                                            pieChart_topicWisePerformance(data);
+
+
+                                                        }
+                                                    });
+
+
+
                                                 });
 
                                             });
@@ -239,5 +291,324 @@ $(document).ready(function() {
         var chart = new Highcharts.Chart(options);
     }
     //*****************************************************************************************
+    //*****************************************************************************
+
+    function columnBasic_studentPerformance(data)
+    {
+
+
+        options = {
+            chart: {
+                renderTo: 'myContent',
+                type: 'column'
+            },
+            title: {
+                text: 'Student Performance'
+            },
+            xAxis: {
+                categories: []
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Percentage(%)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="font-size:10px; color:{series.color};">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: []
+        };
+
+        var subject = [];
+        var test = [];
+        var count_test = 0;
+        $.each(data, function()
+        {
+            if ($.inArray(this.test_name, test) == -1)
+            {
+                test.push(this.test_name);
+            }
+            if ($.inArray(this.subject_name, options.xAxis.categories) == -1)
+            {
+                options.xAxis.categories.push(this.subject_name);
+            }
+        });
+
+        $.each(test, function(i, v)
+        {
+            var entry = {
+                name: v,
+                data: []
+            };
+            $.each(data, function()
+            {
+                if (this.test_name == v)
+                {
+                    entry.data.push(parseFloat(this.percent));
+                }
+
+            });
+            console.log(entry.data);
+            options.series.push(entry);
+        });
+        var chart = new Highcharts.Chart(options);
+    }
+
+
+
+//******************************************************************************
+
+
+    //*************** BAR CHART ********************
+
+    function barChart_topicWisePerformance(data)
+    {
+
+        var options = {
+            chart: {
+                renderTo: 'chart1',
+                type: 'column'
+            },
+            title: {
+                text: 'Topic-wise Performance'
+            },
+            xAxis: {
+                categories: []
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'No of questions'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:f}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: []
+        };
+
+
+
+
+        var topics = {topic: "", t: 0, i: 0, c: 0}; //t-> total, i->incorrect, c->correct
+        var ar_topics = $.makeArray(topics);
+        options.xAxis.categories.push(data[0].topic_name);
+        var counter = 0;
+        var exist;
+        $.each(data, function()
+        {
+            if (!ar_topics[0].topic)
+            {
+                ar_topics[0].topic = this.topic_name;
+            }
+
+            exist = lookupfunction(this.topic_name, ar_topics);
+            if (!exist) {
+                counter++;
+                ar_topics.push({topic: this.topic_name, t: 0, i: 0, c: 0});
+                exist = ar_topics[counter];
+                options.xAxis.categories.push(this.topic_name);
+
+            }
+
+
+            //for total
+            exist.t = exist.t + 1;
+            //////console.log("exist.t", exist.t);
+            //for answered
+            if (this.response != null)
+            {
+                if (this.answer != this.response)
+                {
+                    exist.i = exist.i + 1;
+                }
+
+                if (this.answer == this.response)
+                {
+                    exist.c = exist.c + 1;
+                }
+            }
+
+
+        });
+        ////console.log(options.xAxis.categories);
+
+
+        //top= top.split('\n');
+        var total = {
+            name: 'Total',
+            data: []
+        };
+        var ans = {
+            name: 'Answered',
+            data: []
+        };
+        var c_ans = {
+            name: 'Correctly',
+            data: []
+        };
+        var inc_ans = {
+            name: 'Incorrectly Answered ',
+            data: []
+        };
+        $.each(ar_topics, function(index, value)
+        {
+            total.data.push(parseFloat(this.t));
+            c_ans.data.push(parseFloat(this.c));
+            inc_ans.data.push(parseFloat(this.i));
+            ans.data.push(parseFloat(this.c + this.i));
+        });
+
+        options.series.push(total);
+        options.series.push(ans);
+        options.series.push(c_ans);
+        options.series.push(inc_ans);
+        //console.log(options.series);
+
+
+        var chart = new Highcharts.Chart(options);
+
+    }
+
+
+    //****************find object in list***************
+    function lookupfunction(topic_name, ar_topics)
+    {
+        for (var i = 0, len = ar_topics.length; i < len; i++) {
+
+            if (ar_topics[i].topic == topic_name)
+            {
+                return ar_topics[i]; // Return as soon as the object is found
+
+            }
+        }
+
+        return null; // The object was not found
+
+    }
+
+    //******************PIE CHART***********************
+
+
+    function pieChart_topicWisePerformance(data)
+    {
+        var options = {
+            chart: {
+                renderTo: 'chart2',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'Topic Distribution'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                    type: 'pie',
+                    name: '',
+                    data: []
+                }]
+        };
+
+
+        var topics = {topic: "", t: 0, c: 0}; //t-> total, i->incorrect, c->correct
+
+        var ar_topics = $.makeArray(topics);
+
+        var counter = 0;
+        var exist;
+        $.each(data, function()
+        {
+            if (!ar_topics[0].topic)
+            {
+
+                ar_topics[0].topic = this.topic_name;
+            }
+
+            exist = lookupfunction(this.topic_name, ar_topics);
+            if (!exist) {
+                counter++;
+                ar_topics.push({topic: this.topic_name, t: 0, c: 0});
+                exist = ar_topics[counter];
+
+            }
+
+
+            //for total
+            exist.t = exist.t + 1;
+
+            /*//console.log("exist.t", exist.t);
+             //for answered
+             if (this.response != null)
+             {
+             if (this.answer != this.response)
+             {
+             exist.i = exist.i + 1;
+             }
+             */
+            if (this.answer == this.response)
+            {
+                exist.c = exist.c + 1;
+            }
+            //}
+
+        });
+
+
+        var d = [];
+
+        console.log(ar_topics);
+        var total = 0;
+        $.each(ar_topics, function(index, value)
+        {
+            total += this.t;
+        });
+        $.each(ar_topics, function(index, value)
+        {
+            var d = [];
+            d.push("" + this.topic);
+            d.push((parseFloat(this.t) / parseFloat(total)) * 100);
+            console.log(parseFloat(this.t) / parseFloat(total) * 100);
+            options.series[0].data.push(d);
+        });
+
+        //console.log(options.series[0].data);
+        var chart = new Highcharts.Chart(options);
+    }
 
 });
--- model/student/results.php
+++ model/student/results.php
@@ -20,15 +20,21 @@ if ($f == "get_subject")
     populateSubject();
 }
 
-if( $f == "subject")
+if ($f == "subject")
 {
-   // subject_charts();
+    subject_wise();
 }
 
-if($f == "test")
+if ($f == "t")
 {
-    // testcharts
+    test_summary();
 }
+
+if ($f == "q")
+{
+    question_details();
+}
+
 
 function myPerformance()
 {
@@ -56,7 +62,7 @@ function populateTestCodes()
     $d = date("Y-m-d");
     $x = array();
 
-    $query = "select t.test_id from student_gives_test as st, test as t,academic_year as a where st.user_id='{$_SESSION['user_id']}' and t.test_id=st.test_id and t.subject_name='Math' and t.date<'{$d}' and t.date >=a.acad_start and t.date<=a.acad_end and a.flag_current=1";
+    $query = "select t.test_id from student_gives_test as st, test as t,academic_year as a where st.user_id='{$_SESSION['user_id']}' and t.test_id=st.test_id and t.subject_name='{$_GET['subj']}' and t.date<'{$d}' and t.date >=a.acad_start and t.date<=a.acad_end and a.flag_current=1";
 
     $result = mysqli_query($connection, $query);
 
@@ -76,7 +82,7 @@ function populateSubject()
     require_once("../database.php");
 
     $x = array();
-    
+
     $query = "select distinct(t.subject_name) as s from teaches_class_subject as t,academic_year as a
 where t.standard='{$_SESSION['standard']}' and t.division='{$_SESSION['division']}' and t.acad_year=a.acad_year and a.flag_current=1";
 
@@ -87,6 +93,79 @@ where t.standard='{$_SESSION['standard']}' and t.division='{$_SESSION['division'
         while ($row = mysqli_fetch_assoc($result))
         {
             array_push($x, $row['s']);
+        }
+
+        echo json_encode($x);
+    }
+}
+
+function subject_wise()
+{
+    require_once '../database.php';
+
+    $query = "select t.test_name, round((g.marks_obtained/ g.total_marks *100),2) as percent, t.subject_name from student_gives_test as g , test as t where g.test_id= t.test_id and g.user_id='{$_SESSION['user_id']}'";
+    $result = mysqli_query($connection, $query);
+
+    if ($result)
+    {
+        $x = array();
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            array_push($x, $row);
+        }
+        echo json_encode($x);
+    }
+}
+
+function test_summary()
+{
+    require_once '../database.php';
+
+    $query = "select s.marks_obtained,s.total_marks,t.date,t.subject_name,t.standard,t.division,t.duration from test as t, student_gives_test as s where s.user_id='{$_SESSION['user_id']}' and s.test_id=t.test_id and s.test_id='{$_GET['test_id']}'";
+    $result = mysqli_query($connection, $query);
+
+    if ($result)
+    {
+        $row = mysqli_fetch_assoc($result);
+        echo json_encode($row);
+    }
+}
+
+function question_details()
+{
+    require_once '../database.php';
+
+    $query = "SELECT q.question_id,q.question_desc,r.response,q.answer,q.topic_name,q.type
+FROM response AS r, student_gives_test AS s, question AS q
+WHERE r.response_id = s.response_id
+AND s.user_id =  '{$_SESSION['user_id']}'
+AND r.test_id =  '{$_GET['test_id']}'
+and r.question_id=q.question_id";
+
+    $result = mysqli_query($connection, $query);
+
+    if ($result)
+    {
+        $x = array();
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            if ($row['type'] == "Mcq")
+            {
+
+                $query = "SELECT `optionA`, `optionB`, `optionC`, `optionD` FROM `mcq` WHERE `question_id`='{$row['question_id']}'";
+                $result_temp = mysqli_query($connection, $query);
+
+                if ($result_temp)
+                {
+                    $row_temp = mysqli_fetch_assoc($result_temp);
+                    $row['mcq']['optionA'] = $row_temp['optionA'];
+                    $row['mcq']['optionB'] = $row_temp['optionB'];
+                    $row['mcq']['optionC'] = $row_temp['optionC'];
+                    $row['mcq']['optionD'] = $row_temp['optionD'];
+                }
+
+                array_push($x, $row);
+            }
         }
 
         echo json_encode($x);
